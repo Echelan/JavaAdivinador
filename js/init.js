e35dd04e68da4e75fa8ef29187bf0251d3a1a5c5
Echelan <andres_movilla1103@hotmail.com>
2017-03-31 20:30:53 -0500

    removed materialbox from init
    slight tweak

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 (function($){
   $(function(){
     $('.carousel').carousel({indicators: true});
-    $('.materialboxed').materialbox();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
